b9aee9b0499752cf39ae49cc3d0c237ee13a3d1c
James Streett <103872568+jamesd5V5@users.noreply.github.com>
2023-12-21 14:51:47 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// index.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the element with the ID "app" and set its innerHTML
+    document.getElementById("app").innerHTML = "<h1>Hello, GitHub Pages with JavaScript!</h1>";
+});
